0e0da69da2c99904b44d30a3afaefd36f5578425
Mahelchandupa <mahelchandupa1013@gmail.com>
2025-03-23 16:07:06 +0530

    feat: implement movie fetching functionality with loading and error handling; add MovieCard component for displaying movie details

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,26 @@
 import SearchBar from "@/components/SearchBar";
 import { icons } from "@/constants/icons";
 import { images } from "@/constants/images";
-import { Image, ScrollView, Text, View } from "react-native";
+import {
+  ActivityIndicator,
+  FlatList,
+  Image,
+  ScrollView,
+  Text,
+  View,
+} from "react-native";
 import { useRouter } from "expo-router";
+import useFetch from "@/services/useFetch";
+import { fetchMovies } from "@/services/api";
+import MovieCard from "@/components/MovieCard";
 
 export default function Index() {
-
   const router = useRouter();
+  const {
+    data: movies,
+    loading,
+    error,
+  } = useFetch(() => fetchMovies({ query: "" }));
 
   return (
     <View className=" flex-1 bg-primary">
@@ -16,13 +30,45 @@ export default function Index() {
         contentContainerStyle={{ minHeight: "100%", marginBottom: 10 }}
         className=" flex-1 px-5 "
       >
-        <Image source={icons.logo} className=" w-12 h-10 mt-20 mb-5 mx-auto" />
-        <View className=" flex-1 mt-5">
-          <SearchBar 
-            onPress={() => router.push("/search")}
-            placeholder="Search for a movie"
+        <Image source={icons.logo} className=" w-12 h-10 mt-10 mb-5 mx-auto" />
+
+        {loading ? (
+          <ActivityIndicator
+            size="large"
+            color="#0000ff"
+            className="mt-10 self-center"
           />
-        </View>
+        ) : error ? (
+          <Text className=" text-red-500">Error: {error?.message}</Text>
+        ) : (
+          <View className=" flex-1 mt-5">
+            <SearchBar
+              onPress={() => router.push("/search")}
+              placeholder="Search for a movie"
+            />
+
+            <>
+              <Text className=" text-lg text-white font-bold mt-5 mb-3">
+                Latest Movies
+              </Text>
+
+              <FlatList
+                data={movies}
+                renderItem={({ item }) => <MovieCard {...item} />}
+                keyExtractor={(item) => item.id.toString()}
+                numColumns={3}
+                columnWrapperStyle={{
+                  justifyContent: "flex-start",
+                  gap: 20,
+                  paddingRight: 5,
+                  marginBottom: 10,
+                }}
+                className="mt-2 mb-32"
+                scrollEnabled={false}
+              />
+            </>
+          </View>
+        )}
       </ScrollView>
     </View>
   );
--- services/api.ts
+++ services/api.ts
@@ -1,27 +1,56 @@
-export const TBDB_CONFIG = {
+export const TMDB_CONFIG = {
   BASE_URL: "https://api.themoviedb.org/3",
-  API_KEY: process.env.EXPO_PUBLIC_MOVIE_APP_KEY,
+  API_KEY: process.env.EXPO_PUBLIC_MOVIE_API_KEY,
   headers: {
-    "Content-Type": "application/json",
-    Authorization: `Bearer ${process.env.EXPO_PUBLIC_MOVIE_APP_KEY}`,
+    accept: "application/json",
+    Authorization: `Bearer ${process.env.EXPO_PUBLIC_MOVIE_API_KEY}`,
   },
 };
 
-export const fetchPopularMovies = async ({ query }: { query: string }) => {
+export const fetchMovies = async ({
+  query,
+}: {
+  query: string;
+}): Promise<Movie[]> => {
   const endpoint = query
-    ? `${TBDB_CONFIG.BASE_URL}/search/movie?query=${encodeURIComponent(query)}`
-    : `${TBDB_CONFIG}/discover/movie?sort_by=popularity.desc`;
+    ? `${TMDB_CONFIG.BASE_URL}/search/movie?query=${encodeURIComponent(
+        query
+      )}/&api_key=${TMDB_CONFIG.API_KEY}`
+    : `${TMDB_CONFIG.BASE_URL}/discover/movie?sort_by=popularity.desc&api_key=${TMDB_CONFIG.API_KEY}`;
 
   const response = await fetch(endpoint, {
     method: "GET",
-    headers: TBDB_CONFIG.headers,
+    headers: TMDB_CONFIG.headers,
   });
 
   if (!response.ok) {
-    // @ts-ignore
-    throw new Error("Failed to fetch movies", response.statusText);
+    throw new Error(`Failed to fetch movies: ${response.statusText}`);
   }
 
   const data = await response.json();
   return data.results;
+};
+
+export const fetchMovieDetails = async (
+  movieId: string
+): Promise<MovieDetails> => {
+  try {
+    const response = await fetch(
+      `${TMDB_CONFIG.BASE_URL}/movie/${movieId}?api_key=${TMDB_CONFIG.API_KEY}`,
+      {
+        method: "GET",
+        headers: TMDB_CONFIG.headers,
+      }
+    );
+
+    if (!response.ok) {
+      throw new Error(`Failed to fetch movie details: ${response.statusText}`);
+    }
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error fetching movie details:", error);
+    throw error;
+  }
 };
